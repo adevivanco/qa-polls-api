36f2226d1db56a620d58732ed69adba8eef8f35d
Andres de Vivanco <andy.dv.cm@gmail.com>
2024-01-25 15:58:27 -0500

    add create games test with templatePollUuid by order

--- src/test/java/AdminAPITests.java
+++ src/test/java/AdminAPITests.java
@@ -4,18 +4,18 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static io.restassured.RestAssured.*;
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AdminAPITests {
 
-    String templatePollUuid = "";
+    private static String templatePollUuid = "";
 
 
+    @Order(1)
     @ParameterizedTest
     @CsvSource({"EPL round Round 21,2024-18-17T15:00:00.00+0000"})
     public void createTemplatePoll(String templatePollName, String submissionLimitDate) {
@@ -61,6 +61,7 @@ public class AdminAPITests {
     }
 
 
+    @Order(2)
     @ParameterizedTest
     @CsvSource({"ATP,peru.png", "Chelsea,chelsea.png", "Juventus,juventus.png"})
     public void createTeams(String teamName, String teamImage) {
@@ -109,14 +110,16 @@ public class AdminAPITests {
     }
 
 
+    @Order(3)
     @ParameterizedTest
-    @CsvSource({"Inter,inter.png", "Chelsea,chelsea.png", "Juventus,juventus.png"})
-    public void createGames(String homeTeam, String awayTeam) {
+    @CsvSource({"Inter,Arsenal, 20, 10, 2023-12-17T09:00:00.00+0000",
+                "Newcastle,Arsenal, 20, 10, 2023-12-17T09:00:00.00+0000"})
+    public void createGames(String homeTeam, String awayTeam, int pointsForResult, int pointsForGoals, String gameDateTime) {
 
         String userName = "ninjadelcodigo@gmail.com";
         String password = "emili2K22!";
         String baseUrl = "http://localhost:3335";
-        String templatePollUuid = "26c7eeb4-a28c-4dee-92c2-491137c98d90"; /* TODO: GENERATE DYNAMICALLY */
+        // String templatePollUuid = "26c7eeb4-a28c-4dee-92c2-491137c98d90"; /* TODO: GENERATE DYNAMICALLY */
 
         RestAssured.baseURI = baseUrl;
         RequestSpecification request = RestAssured.given().log().all();
@@ -138,7 +141,31 @@ public class AdminAPITests {
         String token = JsonPath.from(jsonString).get("token");
 
         System.out.println(token);
+        RequestSpecification postGameRequest = RestAssured.given().log().all();
+        postGameRequest.header("Content-Type", "application/json");
+        postGameRequest.header("Authorization", "Bearer " + token);
 
+        System.out.println(templatePollUuid);
+
+        Response postGameResponse = postGameRequest.body(
+            "{ " +
+//                    "\"templatePollUuid\": \""  + "26c7eeb4-a28c-4dee-92c2-491137c98d90" + "\"" + "," +
+                    "\"templatePollUuid\": \""  + templatePollUuid + "\"" + "," +
+                    "\"homeTeamName\": \""  + homeTeam + "\"" + "," +
+                    "\"awayTeamName\": \""  + awayTeam + "\"" + "," +
+                    "\"pointsForResult\": "  + pointsForResult + "," +
+                    "\"pointsForGoals\": "  + pointsForGoals  + "," +
+                    "\"gameDateTime\": \""  + gameDateTime + "\"" + "," +
+                    "\"phase\": \""  + "Groups" + "\""  +
+                " }")
+            .post("/template-games");
+
+        postGameResponse.then().log().all();
+        Assertions.assertEquals(postGameResponse.getStatusCode(), 200);
+
+        jsonString = postGameResponse.asString();
+        Assertions.assertEquals(JsonPath.from(jsonString).get("data.homeTeamName"), homeTeam);
+        Assertions.assertEquals(JsonPath.from(jsonString).get("data.awayTeamName"), awayTeam);
     }
 }
 
